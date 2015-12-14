6175f57d5f2437846478d13148c14f45a51345df
koushik22 <gattukaushik@gmail.com>
2015-12-14 17:06:17 -0600

    Initial Projecct

---
+++ Lesson01/src/com/kau/learn/domain/Person.java
@@ -0,0 +1,44 @@
+package com.kau.learn.domain;
+
+import java.util.Calendar;
+
+public class Person {
+	String id;
+	String firstName;
+	String lastName;
+	Calendar dateOfBirth;
+	Address address;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Calendar getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Calendar dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
+}
