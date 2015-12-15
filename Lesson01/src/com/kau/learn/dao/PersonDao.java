256c3e60898c8d70053fa24bdc1191bb03d49319
skgthknd <sndepguthikonda@gmail.com>
2015-12-14 19:17:24 -0500

    Added comment for PersonDao.java

--- Lesson01/src/com/kau/learn/dao/PersonDao.java
+++ Lesson01/src/com/kau/learn/dao/PersonDao.java
@@ -2,6 +2,7 @@ package com.kau.learn.dao;
 
 import com.kau.learn.domain.Person;
 
+// This is public Class
 public interface PersonDao {
 	public void create(Person person);
 	public void delete(String personId);
