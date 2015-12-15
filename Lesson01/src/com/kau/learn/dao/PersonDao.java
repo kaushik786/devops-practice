e652fa10769ed74e463b0f88bd52fa5b033307ca
koushik22 <gattukaushik@gmail.com>
2015-12-14 18:20:03 -0600

    Adding comment

--- Lesson01/src/com/kau/learn/dao/PersonDao.java
+++ Lesson01/src/com/kau/learn/dao/PersonDao.java
@@ -2,6 +2,7 @@ package com.kau.learn.dao;
 
 import com.kau.learn.domain.Person;
 
+// I'm adding my comment
 public interface PersonDao {
 	public void create(Person person);
 	public void delete(String personId);
