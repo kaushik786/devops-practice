c40787e8f768464907208e79d8aafb84db43e08b
skgthknd <sndepguthikonda@gmail.com>
2015-12-14 19:31:39 -0500

    test for merge

--- Lesson01/src/com/kau/learn/dao/PersonDao.java
+++ Lesson01/src/com/kau/learn/dao/PersonDao.java
@@ -3,6 +3,7 @@ package com.kau.learn.dao;
 import com.kau.learn.domain.Person;
 
 // This is public Class
+// This is a coomment for testing merge.
 public interface PersonDao {
 	public void create(Person person);
 	public void delete(String personId);
